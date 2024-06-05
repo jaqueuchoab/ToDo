8c3547a5e53ee40640d42df79eccc600b1711adb
jaqueuchoab <jaqueline.uchoab@gmail.com>
2024-06-05 17:14:49 -0300

    Tipagem

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
+import React from 'react';
 import './App.css';
 import Card from './components/Card';
 
+export type Todo = {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 function App() {
+  const [todos, setTodos] = React.useState<Todo[]>([]);
+
   return (
-    <div>
-      <Card title='Fazer Café'/>
+    <div className="App">
+      <div className="add-todo">
+        <input type="text" placeholder="Fazer café" />
+        <button>Adicionar</button>
+      </div>
+
+      {todos.map((todo: Todo) => {
+        return <Card key={todo.id} todo={todo} />;
+      })}
     </div>
   );
 }
--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,10 +1,21 @@
 // Organização de pasta diferente: Para cada componente cria-se uma pasta, o componente principal se chamará index, e os demais podem ser estilos ou pequenas partes deste componente. Portanto, na importanção o nome do arquivo fica implicito e o que vale é o export.
-type CardProps = {
-  title: string;
-}
+import { Todo } from '../../App';
+import './style.css';
 
-const Card = ({ title } : CardProps) => {
-  return <div>{title}</div>;
+type CardProps = {
+  todo: Todo;
+};
+
+const Card = ({ todo }: CardProps) => {
+  return (
+    <div className="card">
+      <h2>{todo.title}</h2>
+      <div className="card-buttons">
+        <button>Completar</button>
+        <button>Deletar</button>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
